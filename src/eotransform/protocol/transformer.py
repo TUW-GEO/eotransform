a94dfd361564c3746de58f07a6f2dcf541312b59
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-08-09 19:10:18 +0200

    F!! add predicated transformer and apply to collection

--- src/eotransform/protocol/transformer.py
+++ src/eotransform/protocol/transformer.py
@@ -9,3 +9,18 @@ class Transformer(Generic[TransformerT, TransformerU]):
     @abstractmethod
     def __call__(self, x: TransformerT) -> TransformerU:
         ...
+
+
+PredicatedTransformerKey = TypeVar('PredicatedTransformerKey')
+PredicatedTransformerIn = TypeVar('PredicatedTransformerIn')
+PredicatedTransformerOut = TypeVar('PredicatedTransformerOut')
+
+
+class PredicatedTransformer(Generic[PredicatedTransformerKey, PredicatedTransformerIn, PredicatedTransformerOut]):
+    @abstractmethod
+    def is_applicable(self, k: PredicatedTransformerKey) -> bool:
+        ...
+
+    @abstractmethod
+    def apply(self, k: PredicatedTransformerKey, x: PredicatedTransformerIn) -> PredicatedTransformerOut:
+        ...
