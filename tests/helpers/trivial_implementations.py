1b80382581a0d1e18c08a841cab37e3519eca8e2
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-08-09 17:25:31 +0200

    F!! add base transformers

--- tests/helpers/trivial_implementations.py
+++ tests/helpers/trivial_implementations.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from eotransform.protocol.transformer import Transformer
 
 
@@ -7,3 +9,19 @@ class Add(Transformer[int, int]):
 
     def __call__(self, x: int) -> int:
         return x + self._v
+
+
+class Mul(Transformer[int, int]):
+    def __init__(self, v):
+        self._v = v
+
+    def __call__(self, x: int) -> int:
+        return x * self._v
+
+
+class ReplaceWithString(Transformer[Any, str]):
+    def __init__(self, string: str):
+        self._string = string
+
+    def __call__(self, x: Any) -> str:
+        return self._string
