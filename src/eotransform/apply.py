4e79cc184d4a3d40bff569759360433827f287f0
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-08-13 11:33:23 +0200

    d add doc string to Apply

--- src/eotransform/apply.py
+++ src/eotransform/apply.py
@@ -7,6 +7,13 @@ ApplyU = TypeVar('ApplyU')
 
 
 class Apply(Transformer[ApplyT, ApplyU]):
+    """
+    Applies an arbitrary function to the input which takes the input as argument and returns a transformed output
+
+    >>> Apply(lambda x: f"The answer is: {x}")(42)
+    'The answer is: 42'
+    """
+
     def __init__(self, fn: Callable[[ApplyT], ApplyU]):
         self._fn = fn
 
