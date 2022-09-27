5500adcf407fb198810cae69d8a9e86740ce7284
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-09-27 13:30:59 +0200

    t  handle imprecise measurements on IOs

--- tests/test_utilities_profiling.py
+++ tests/test_utilities_profiling.py
@@ -23,51 +23,56 @@ def set_logging_active():
 
 
 def test_measure_scope(clock, slow_factor):
-    with clock.measure():
-        time.sleep(0.01 * slow_factor)
+    for _ in range(5):
+        with clock.measure():
+            time.sleep(0.01 * slow_factor)
 
     assert clock.mean_measures == approx(0.01 * slow_factor, abs=0.009 * slow_factor)
 
 
 def test_measure_multiple_scopes_and_provide_mean(clock, slow_factor):
-    with clock.measure():
-        time.sleep(0.01 * slow_factor)
-    with clock.measure():
-        time.sleep(0.03 * slow_factor)
+    for _ in range(5):
+        with clock.measure():
+            time.sleep(0.01 * slow_factor)
+        with clock.measure():
+            time.sleep(0.03 * slow_factor)
 
     assert clock.mean_measures == approx(0.02 * slow_factor, abs=0.005 * slow_factor)
 
 
 def test_drop_first(clock, slow_factor):
     with clock.measure():
-        time.sleep(0.01 * slow_factor)
-    with clock.measure():
-        time.sleep(0.03 * slow_factor)
+        time.sleep(0.05 * slow_factor)
 
-    assert clock.drop_first().mean_measures == approx(0.03 * slow_factor, abs=0.005 * slow_factor)
+    for _ in range(5):
+        with clock.measure():
+            time.sleep(0.01 * slow_factor)
+
+    assert clock.drop_first().mean_measures == approx(0.01 * slow_factor, abs=0.005 * slow_factor)
 
 
 def test_total_measures(clock, slow_factor):
-    with clock.measure():
-        time.sleep(0.01 * slow_factor)
-    with clock.measure():
-        time.sleep(0.03 * slow_factor)
+    for _ in range(5):
+        with clock.measure():
+            time.sleep(0.01 * slow_factor)
+        with clock.measure():
+            time.sleep(0.03 * slow_factor)
 
-    assert clock.total_measures == approx(0.04 * slow_factor, abs=0.005 * slow_factor)
+    assert clock.total_measures == approx(0.04 * 5 * slow_factor, abs=0.01 * slow_factor)
 
 
 def test_clock_can_have_a_name():
     assert PerformanceClock("a name").name == "a name"
 
 
-def test_performance_clock_decorators(caplog, set_logging_active, slow_factor):
+def test_performance_clock_decorators(caplog, set_logging_active):
     @clock_performance("process")
     def some_process():
-        time.sleep(0.01 * slow_factor)
+        time.sleep(0.01)
 
     @clock_performance("io")
     def some_io():
-        time.sleep(0.03 * slow_factor)
+        time.sleep(0.03)
 
     log("process").every(10)
     log("io").every(1)
@@ -80,11 +85,11 @@ def test_performance_clock_decorators(caplog, set_logging_active, slow_factor):
         assert sum("some_process" in r.msg for r in caplog.records) == 1
 
 
-def test_performance_clock_on_class_methods(caplog, set_logging_active, slow_factor):
+def test_performance_clock_on_class_methods(caplog, set_logging_active):
     class SomeClass:
         @clock_performance("process")
         def some_method(self):
-            time.sleep(0.01 * slow_factor)
+            time.sleep(0.01)
 
     log("process").every(1)
 
@@ -94,10 +99,10 @@ def test_performance_clock_on_class_methods(caplog, set_logging_active, slow_fac
         assert sum("some_method" in r.msg for r in caplog.records) == 1
 
 
-def test_ignore_performance_logging_if_environment_variable_is_not_set(caplog, slow_factor):
+def test_ignore_performance_logging_if_environment_variable_is_not_set(caplog):
     @clock_performance("process")
     def some_process():
-        time.sleep(0.01 * slow_factor)
+        time.sleep(0.01)
 
     log("process").every(1)
 
