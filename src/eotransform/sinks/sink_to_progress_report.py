1e981fe10575606835378bd59a753be1f7529624
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-03-07 10:56:53 +0100

    F! add progress reporter + interface

---
+++ src/eotransform/sinks/sink_to_progress_report.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod
+from typing import Any, Optional, Protocol
+
+from eotransform.protocol.sink import Sink
+
+
+class Reporter(Protocol):
+    @abstractmethod
+    def update(self, n: Optional[int] = 1) -> None:
+        ...
+
+
+class SinkToProgressReport(Sink[Any]):
+    def __init__(self, reporter: Reporter):
+        self._reporter = reporter
+
+    def __call__(self, x: Any) -> None:
+        self._reporter.update()
